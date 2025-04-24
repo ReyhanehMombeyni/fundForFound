89634577324dd62b9688350ca8e234b307c0fb65
Reyhaneh <reyhaneh.sa.mo@gmail.com>
2025-04-24 12:29:25 +0330

    update: smtp setting in plugin.ts

--- my-project/config/plugins.ts
+++ my-project/config/plugins.ts
@@ -5,3 +5,24 @@ export default ({ env }) => ({
     },
   },
 });
+
+module.exports = {
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: 'smtp.gmail.com',
+        port: 587, 
+        auth: {
+          user: 'reyhaneh.sa.mo@gmail.com', 
+          pass: 'cutn lhld swen dxcs', 
+        },
+      },
+      settings: {
+        defaultFrom: 'reyhaneh.sa.mo@gmail.com', 
+        defaultReplyTo: 'reyhaneh.sa.mo@gmail.com',
+      },
+    },
+  },
+};
+
